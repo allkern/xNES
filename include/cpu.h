6a141fae555838a94f07e6703352de74d2ae29f6
Astherix <aurealinbox@gmail.com>
2019-09-24 00:56:38 -0300

    Added some signals, and status manipulation facilities

--- include/cpu.h
+++ include/cpu.h
@@ -19,19 +19,24 @@ class cpu {
 				   opcode = 0,   // opcode: Contains the last fetched opcode
 				   cycles = 0,   // cycles: Contains the amount of cycles needed to be consumed before the next instruction
 				   addr_abs = 0; // addr_abs: Contains the last fetched absolute address
+					 next = this->pc + 1;
 	int_least16_t  addr_rel = 0; // addr_rel: Contains the last fetched relative address
 
 	// Bus:
 	bus* Bus = defaultBus.getThisReference();
+	inline void sf(uint_least8_t m) { this->p |= m; } // Set flag
+	inline void rf(uint_least8_t m) { this->p &= ~m; } // Reset flag
+	inline void tf(uint_least8_t m) { this->p ^= m; } // Toggle flag
 
 	// Addressing modes:
+	#define next this->pc + 1
 	addrmode(imp, false, this->a   								   , 0, 0, fetched,); // Implied
 	addrmode(acc, false, this->a   								   , 0, 0, fetched,); // Accumulator
-	addrmode(imm, true , this->pc+1								   , 0, 1, fetched,); // Immediate
-	addrmode(zp0, true , this->pc+1								   , 0, 1, fetched,); // Zero Page
-	addrmode(abs, false, rw(this->pc+1):, 0, 2, addr_abs,); // Absolute
-	addrmode(rel, false, (int_least16_t)rb(this->pc+1), 0, 1, addr_rel,); // Relative
-	addrmode(ind, true , rw(this->pc+1), 0, 2, fetched,); // Indirect
+	addrmode(imm, true , next								   , 0, 1, fetched,); // Immediate
+	addrmode(zp0, true , next								   , 0, 1, addr_abs,); // Zero Page
+	addrmode(abs, false, rw(next):, 0, 2, addr_abs,); // Absolute
+	addrmode(rel, false, (int_least16_t)rb(next), 0, 1, addr_rel,); // Relative
+	addrmode(ind, true , rw(next), 0, 2, fetched,); // Indirect
 
 	// Instructions:
 	#define ld(r) this->##r = this->fetched
@@ -39,6 +44,32 @@ class cpu {
 	instruction(ldx, imm, ld(x);, 2, 1);
 	instruction(ldy, imm, ld(y);, 2, 1);
 	instruction(ora, imm, this->a |= this->fetched;, 2, 1);
+
+	// Signals:
+	void clk() {
+		if (this->cycles > 0) {
+			--cycles;
+		} else {
+			sf(flags::u);
+		}
+	}
+
+	void rst() {
+		this->pc = rw(0xfffc);
+		this->a  = this->x = this->y = 0;
+		this->sp = 0xfd;
+		sf(flags::u);
+		fetched  = 0;
+		cycles   = 8;
+	}
+
+	void irq() {
+		// To-do
+	}
+
+	void nmi() {
+		// To-do
+	}
 
 public:
 	inline void initializeMemoryFromString(std::string s, int base) {
@@ -52,7 +83,7 @@ public:
 
 	#define fhex(n, f) std::setfill('0') << std::setw(f) << std::hex << n
 	inline void executeOne() {
-		uint_least8_t opcode = read(this->pc);
+		this->opcode = read(this->pc);
 		switch (opcode) {
 			case 0xa9: _instruction(lda, imm); break;
 			case 0xa2: _instruction(ldx, imm); break;
